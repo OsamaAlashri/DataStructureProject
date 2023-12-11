92d5347d8280f1692484f12f43e85223637f8d09
microburg <130839467+microburg@users.noreply.github.com>
2023-12-11 22:59:29 +0200

    Add files via upload

--- Station.cpp
+++ Station.cpp
@@ -1,5 +1,33 @@
 #include "Station.h"
-bool Station::CheckTime()
-{
-	return false;
+
+Station::Station(int stationNum) : stationNumber(stationNum) {}
+
+void Station::addBus(const Bus& bus) {
+    availableBuses.add(bus);
 }
+
+void Station::removeBus(const Bus& bus) {
+    availableBuses.remove(bus);
+}
+
+List<Bus> Station::getAvailableBuses() const {
+    return availableBuses;
+}
+
+void Station::addPassenger(const Passengers& passenger) {
+    waitingPassengers.add(passenger);
+}
+
+void Station::removePassenger(const Passengers& passenger) {
+    waitingPassengers.remove(passenger);
+}
+
+List<Passengers> Station::getWaitingPassengers() const {
+    return waitingPassengers;
+}
+
+void Station::unloadLoadPassengers() {
+
+}
+
+
--- Station.h
+++ Station.h
@@ -1,20 +1,35 @@
-
-#include "Queue.h"
+#pragma once
 #include "List.h"
+#include "Bus.h"
 #include "Passengers.h"
-#include <iostream>
+#include "Queue.h"
 
 class Station {
-	int stationNumber;
+private:
+    int stationNumber;
+    List<Passengers> waitingPassengers;
+    List<Bus> availableBuses;
 	int maxStations;
+
 public:
+    Station(int stationNum);
+
+    void addBus(const Bus& bus);
+    void removeBus(const Bus& bus);
+    List<Bus> getAvailableBuses() const;
+
+    void addPassenger(const Passengers& passenger);
+    void removePassenger(const Passengers& passenger);
+    List<Passengers> getWaitingPassengers() const;
+
+    void unloadLoadPassengers();
 
 	List<Passengers> Np; //normal person cal leave the queue after MaxW time (list is needed not queue)
 	Queue<Passengers> Aged;
 	Queue<Passengers> POD;
 	Queue<Passengers> PW;
 	Queue<Passengers> Wp;
-	bool CheckTime(); //checks if in working hours
-
+	bool CheckTime();
 
 };
+
