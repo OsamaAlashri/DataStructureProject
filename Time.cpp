62d62e75d902566c26583ee0ae2f3958c8078ec5
Mohamed-R-O <151867136+Mohamed-R-O@users.noreply.github.com>
2023-12-11 15:37:13 +0300

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,30 +1,33 @@
 #include "Time.h"
 
-Time::Time() : hour(0), minute(0) {}
+Time::Time() : hour(0), minute(0), second(0) {} // initialize seconds to 0
 
-void Time::setTime(int h, int m) {
+void Time::setTime(int h, int m, int s) {
     if (h >= 0 && h < 24) hour = h;
     if (m >= 0 && m < 60) minute = m;
+    if (s >= 0 && s < 60) second = s; 
 }
-
 
 void Time::printTime() {
     std::cout << std::setfill('0') << std::setw(2) << hour << ":"
-        << std::setfill('0') << std::setw(2) << minute << std::endl;       //output formatting in a (00:00) format.
+        << std::setfill('0') << std::setw(2) << minute << std::endl; 
 }
 
-
-
 void Time::tick() {
-    ++minute;
-    if (minute >= 60) {
-        minute = 0;
-        ++hour;
-        if (hour >= 24) {
-            hour = 0;
+    ++second;
+    if (second >= 60) {
+        second = 0;
+        ++minute;
+        if (minute >= 60) {
+            minute = 0;
+            ++hour;
+            if (hour >= 24) {
+                hour = 0;
+            }
         }
     }
 }
 
 int Time::getHour() const { return hour; }
 int Time::getMinute() const { return minute; }
+int Time::getSecond() const { return second; } 
