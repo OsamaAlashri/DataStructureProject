e75c47b8df849da6de918422437e14a33cb334c6
OsamaAlashri <121246298+OsamaAlashri@users.noreply.github.com>
2023-12-11 22:55:55 +0200

    Update UI.h

--- UI.h
+++ UI.h
@@ -3,11 +3,6 @@
 
 class UI {
 public:
-    // Function to start the interactive mode
     static void startInteractiveMode(const List<Station>& stations);
-
-    // Function to display the status of the system at a specific station and time
     static void displayStationStatus(const Station& station, int time);
-
-    // Other input/output functions as needed
 };
