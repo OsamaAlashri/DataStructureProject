12862141c1c286f4cfb2fc9a1cfaf71fd241becf
OsamaAlashri <121246298+OsamaAlashri@users.noreply.github.com>
2023-12-11 22:55:23 +0200

    Update UI.cpp

--- UI.cpp
+++ UI.cpp
@@ -4,12 +4,10 @@
 void UI::startInteractiveMode(const List<Station>& stations) {
     for (int i = 0; i < stations.getSize(); ++i) {
         Station currentStation = stations.getElement(i);
-        int currentTime = 0;  // You can replace this with the actual time value
+        int currentTime = 0;  
 
-        // Display the status of the system at the current station and time
         displayStationStatus(currentStation, currentTime);
-
-        // Wait for user input to proceed to the next station
+        
         std::cout << "Press Enter to continue...";
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cin.get();  // This line is to capture the Enter key press
@@ -17,10 +15,8 @@ void UI::startInteractiveMode(const List<Station>& stations) {
 }
 
 void UI::displayStationStatus(const Station& station, int time) {
-    // Display the status of the system at a specific station and time
     std::cout << "Time: " << time << std::endl;
 
-    // Display waiting passengers
     List<Passengers> waitingPassengers = station.getWaitingPassengers();
     std::cout << "Waiting Passengers:" << std::endl;
     for (int i = 0; i < waitingPassengers.getSize(); ++i) {
@@ -28,7 +24,6 @@ void UI::displayStationStatus(const Station& station, int time) {
         std::cout << "Passenger ID: " << passenger.getID() << " Direction: " << passenger.getDirection() << std::endl;
     }
 
-    // Display available buses
     List<Bus> availableBuses = station.getAvailableBuses();
     std::cout << "Available Buses:" << std::endl;
     for (int i = 0; i < availableBuses.getSize(); ++i) {
@@ -36,7 +31,6 @@ void UI::displayStationStatus(const Station& station, int time) {
         std::cout << "Bus ID: " << bus.getID() << " Direction: " << bus.getDirection()
                   << " Type: " << bus.getType() << " Capacity: " << bus.getCapacity() << std::endl;
 
-        // Display IDs of passengers on the bus
         List<Passengers> passengersOnBus = bus.getPassengers();
         std::cout << "Passengers on Bus:";
         for (int j = 0; j < passengersOnBus.getSize(); ++j) {
@@ -45,8 +39,6 @@ void UI::displayStationStatus(const Station& station, int time) {
         std::cout << std::endl;
     }
 
-    // Display other relevant information
-    // ...
 
     std::cout << "------------------------" << std::endl;
 }
