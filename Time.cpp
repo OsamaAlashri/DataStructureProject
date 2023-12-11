2750645e1e3afd775561a71d7575d92bab8109c2
Mohamed-R-O <151867136+Mohamed-R-O@users.noreply.github.com>
2023-12-11 15:19:31 +0300

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,17 +1,19 @@
 #include "Time.h"
-using namespace std;
 
 Time::Time() : hour(0), minute(0) {}
 
 void Time::setTime(int h, int m) {
-    hour = (h >= 0 && h < 24);
-    minute = (m >= 0 && m < 60);
+    if (h >= 0 && h < 24) hour = h;
+    if (m >= 0 && m < 60) minute = m;
 }
 
+
 void Time::printTime() {
-    cout << setfill('0') << setw(2) << hour << ":"
-        << setfill('0') << setw(2) << minute << endl;       //output formatting in a (00:00) format.
+    std::cout << std::setfill('0') << std::setw(2) << hour << ":"
+        << std::setfill('0') << std::setw(2) << minute << std::endl;       //output formatting in a (00:00) format.
 }
+
+
 
 void Time::tick() {
     ++minute;
@@ -23,3 +25,6 @@ void Time::tick() {
         }
     }
 }
+
+int Time::getHour() const { return hour; }
+int Time::getMinute() const { return minute; }
