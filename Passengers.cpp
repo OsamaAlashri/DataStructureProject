f5fcf8145505b0fa10e46b3e3cf7efce90e08d2f
Mohamed-R-O <151867136+Mohamed-R-O@users.noreply.github.com>
2023-12-23 15:25:08 +0300

    Update Passengers.cpp // small typos fixed

--- Passengers.cpp
+++ Passengers.cpp
@@ -8,11 +8,11 @@ void Passengers::setGetOnOff(Time t) { getON_OFF = t; }
 void Passengers::setStartStation(int s) { startStation = s; }
 void Passengers::setEndStation(int e) { endStation = e; }
 void Passengers::setType(PassengerType t) { type = t; }
-void Passengers::setID(int I) {ID = I}
+void Passengers::setID(int I) {ID = I; }
 
 Time Passengers::getArrivalTime() const { return arrivalTime; }
 Time Passengers::getGetOnOff() const { return getON_OFF; }
 int Passengers::getStartStation() const { return startStation; }
 int Passengers::getEndStation() const { return endStation; }
 PassengerType Passengers::getType() const { return type; }
-int passengers::getID() const {return ID;}
+int Passengers::getID() const {return ID;}
